f867342130632c2f34e6a44c3156be6ea892355b
ebreton <email>
2018-05-28 20:16:16 +0200

    reset versions.py

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.2.0-11-g0620b66'
+_release = '0.1.0'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.2.1-rc'
+_version = '0.1.1-rc'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '0620b6603ddcb0235fa2207671e85f247ac69697'
+_build = '0f9875ebb797a219e19a617d55dbe244b63498ce'
