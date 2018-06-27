2ac970e9d920a807272a710b6fa1c49106bbb7fe
ebreton <email>
2018-06-27 08:52:18 +0200

    updated CHANGELOG (and version numbers)

--- versions.py
+++ versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.1.0-13-ge354f5c'
+_release = '0.1.0-14-g6ba6ae4'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.1.1-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'e354f5cec70857551d28a6c4e7519212035e20e9'
+_build = '6ba6ae4e6cadfa2ebedcacff642bb53329f9a099'
